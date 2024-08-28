ee449de10f901142fdfda0cbf3d29763cd06a7a3
반영환 <lopahn2@gmail.com>
2024-08-28 22:10:44 +0900

    delete aws configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "access_key" {
   description = "AWS account access key"
   type        = string
-  default     = "AKIA4J65KRO2WEUKYDRX"
+  default     = ""
 }
 
 variable "secret_key" {
   description = "AWS account secret key"
   type        = string
-  default     = "0iJNr/gVG9+oFb93XPWYW3/ZD7tvL/P5awgIj9o+"
+  default     = ""
 }
